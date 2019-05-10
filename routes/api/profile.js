6dff56629565a04ad3293481601c1c7edb47a27f
dergarcon <nick@om90.de>
2019-05-10 17:29:51 +0200

    added route: PUT experience in profile IN profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -196,4 +196,57 @@ router.delete('/', auth, async (req, res) => {
 
 
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', [auth,
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From Date is required').not().isEmpty(),
+    check('location', 'Location is required').not().isEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        })
+        //unshift() is like push() but pushes the new item to the top
+        profile.experience.unshift(newExp)
+        await profile.save()
+        res.json(profile)
+
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+
 module.exports = router

04c44256e2294fe7e39e8ddd2901bcbdf564d115
dergarcon <nick@om90.de>
2019-05-10 16:44:21 +0200

    added routes: get all profiles & get profile by userID IN routes/api/profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -88,9 +88,6 @@ router.post('/',
         if (linkedin) profileFields.social.linkedin = linkedin
         if (xing) profileFields.social.xing = xing
 
-        console.log(profileFields.social)
-        console.log(profileFields.social.twitter)
-
         try {
             let profile = await Profile.findOne({
                 user: req.user.id
@@ -120,6 +117,46 @@ router.post('/',
 
 
     })
+
+
+// @route   POST api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+
+// @route   POST api/profile/user/:user_id
+// @desc    Get Profile by user ID
+// @access  Public
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar'])
+        if (!profile) return res.status(400).json({
+            msg: 'Profile not found.'
+        })
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind == 'ObjectID') {
+            return res.status(400).json({
+                msg: 'Profile not found.'
+            })
+        }
+        res.status(500).send('Server Error')
+    }
+})
 
 
 

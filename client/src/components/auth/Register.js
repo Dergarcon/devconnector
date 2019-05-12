8426b2e80138bdb907564d19b99acbe4ca051187
dergarcon <nick@om90.de>
2019-05-12 11:40:35 +0200

    Login User and redirect to dashboard if logged in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,19 +1,24 @@
 import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { setAlert } from '../../actions/alert'
 import { register } from '../../actions/auth'
 import PropTypes from 'prop-types'
 
 // import axios from 'axios'  - was required for commented stuff (request without redux)
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
         password2: ''
     })
+
+    // redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="dashboard" />
+    }
 
     const { name, email, password, password2 } = formData
     const onChange = e => setFormData({
@@ -93,6 +98,13 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
     register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
 }
 
-export default connect(null, { setAlert, register })(Register)
+const mapStateToProps = state => ({
+
+    isAuthenticated: state.auth.isAuthenticated,
+
+})
+
+export default connect(mapStateToProps, { setAlert, register })(Register)

5650ca9538aa51e9e8a410c885de0f0cb997802e
dergarcon <nick@om90.de>
2019-05-14 05:24:13 +0200

    created <PrivateRoute>

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
+
 
 
 // Redux
@@ -37,6 +40,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/login" component={Login}></Route>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
             </Switch>
           </section>
         </Fragment>

49484d602bb2c857c8ede8d78ea617f7b24568dc
dergarcon <nick@om90.de>
2019-05-19 12:19:31 +0200

    display all profiles

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-forms/EditProfile'
 import AddExperience from './components/profile-forms/AddExperience'
 import AddEducation from './components/profile-forms/AddEducation'
 import PrivateRoute from './components/routing/PrivateRoute'
+import Profiles from './components/profiles/Profiles'
 
 
 
@@ -44,6 +45,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/login" component={Login}></Route>
+              <Route exact path="/profiles" component={Profiles}></Route>
               <PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
               <PrivateRoute exact path="/create-profile" component={CreateProfile}></PrivateRoute>
               <PrivateRoute exact path="/edit-profile" component={EditProfile}></PrivateRoute>
